3f6004d26cbc791b2ad0cc5acc6494a2755d1800
Shivam Kale <shivam.kale@infosys.com>
2022-12-15 15:31:58 +0530

    This is my first commit

---
+++ src/DemoGit/Test.java
@@ -0,0 +1,16 @@
+package DemoGit;
+
+public class Test {
+ 
+	public static void main(String[] args) {
+		
+		String hungry = "yes";
+		
+		if(hungry=="yes");
+		System.out.println("eat something.......");	
+		{
+			System.out.println("Do work.......");
+		}
+	}
+	
+}
